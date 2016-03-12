f3d6fd7dc9fa5e5484d32730d1c6219f56d9583a
Jacqueline Wijaya <jackiewijaya@webmocha.com>
2016-03-11 16:00:24 -1000

    Resolving "Module not found: Error: Cannot resolve module" errors for react-intl locale-data modules.

--- src/js/index.js
+++ src/js/index.js
@@ -6,14 +6,16 @@ require("leaflet/dist/leaflet.css");
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { IntlProvider, addLocaleData } from 'react-intl';
+import { en, pt, ja, zh } from 'react-intl/locale-data';
+
 import { getCurrentLocale, getLocaleData } from 'grommet/utils/Locale';
 import PeopleFinder from './components/PeopleFinder';
 
 const locale = getCurrentLocale();
-addLocaleData(require('react-intl/lib/locale-data/en'));
-addLocaleData(require('react-intl/lib/locale-data/pt'));
-addLocaleData(require('react-intl/lib/locale-data/ja'));
-addLocaleData(require('react-intl/lib/locale-data/zh'));
+addLocaleData(en);
+addLocaleData(pt);
+addLocaleData(ja);
+addLocaleData(zh);
 
 let messages;
 try {
